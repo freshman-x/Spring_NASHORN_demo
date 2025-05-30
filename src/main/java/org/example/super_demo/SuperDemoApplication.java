9d0ec33ec6b0226b636e51e88d32580b28e4bf48
xuyongkang <xuyongkang.xyk@alibaba-inc.com>
2025-05-30 18:07:11 +0800

    1

---
+++ src/main/java/org/example/super_demo/SuperDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.super_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/super_demo/SuperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.super_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
